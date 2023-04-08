189e1243b1ac296a975967f4e40b3c42c128601c
baymax99 <2855862477@qq.com>
2023-04-08 21:28:46 +0800

    enum

--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -1,14 +1,14 @@
 // enums2.rs
 // Execute `rustlings hint enums2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
     // TODO: define the different variants used below
-    Move(x, y),
+    // 结构体
+    Move{x: i32, y: i32},
     Echo(String),
-    ChangeColor(a, b, c),
+    // 元组
+    ChangeColor(i32, i32, i32),
     Quit
 }
 
@@ -19,7 +19,7 @@ impl Message {
 }
 
 fn main() {
-    let messages = [
+    let messages: [Message; 4]= [
         Message::Move { x: 10, y: 30 },
         Message::Echo(String::from("hello world")),
         Message::ChangeColor(200, 255, 255),
