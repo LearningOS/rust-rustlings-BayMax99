44e832b01106c5f07f2120ead22f9d9af916ac32
baymax99 <2855862477@qq.com>
2023-04-10 23:50:11 +0800

    ninth day

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -30,11 +30,12 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: String) -> Vec<> {
         // TODO: Complete the output declaration!
         let mut output: ??? = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            
         }
         output
     }
@@ -43,7 +44,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we have to import to have `transformer` in scope?
-    use ???;
+    use my_module::transformer;
     use super::Command;
 
     #[test]
