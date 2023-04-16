068bff20453d90a14e90d43cea3cd8ea9c1fd298
baymax99 <2855862477@qq.com>
2023-04-16 20:32:25 +0800

    update

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -7,12 +7,13 @@
 // Execute `rustlings hint clippy1` or use the `hint` watch subcommand for a hint.
 
 use std::f32;
+use std::f32::consts::PI;
 
 fn main() {
-    let pi:f32 = 3.14;
+    // let pi:f32 = 3.14;
     let radius:f32 = 5.00;
 
-    let area = pi * f32::powi(radius, 2);
+    let area = PI * f32::powi(radius, 2);
 
     println!(
         "The area of a circle with radius {:.2} is {:.5}!",
